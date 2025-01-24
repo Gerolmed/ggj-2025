9b073f8592f5913575055ca883cbe8349d8cc3cb
johannesjumpertz <johannes.jumpertz@isys.de>
2025-01-24 22:55:22 +0100

    Added simple scene swap

--- code/game.cpp
+++ code/game.cpp
@@ -5,6 +5,7 @@
 #include "stb_ds.h"
 
 #include "raylib.h"
+#include "player.cpp"
 
 typedef uint8_t u8;
 typedef uint32_t u32;
@@ -21,17 +22,41 @@ struct Room
     Tile tiles[20 * 15];
 };
 
+enum SceneMode
+{
+    SCENE_MODE_TEST_DEFAULT,
+    SCENE_MODE_TEST_PLAYER,
+};
+
 i32 main()
 {
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
     InitWindow(1600, 900, "Divegame");
     SetTargetFPS(60);
 
+    SceneMode sceneMode = SCENE_MODE_TEST_DEFAULT;
+
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_F1))
+        {
+            sceneMode = SCENE_MODE_TEST_DEFAULT;
+        } else if (IsKeyPressed(KEY_F2))
+        {
+            sceneMode = SCENE_MODE_TEST_PLAYER;
+        }
+
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawRectangle(100, 100, 100, 100, RED);  
+        switch (sceneMode)
+        {
+            case SCENE_MODE_TEST_DEFAULT:
+                DrawRectangle(100, 100, 100, 100, RED);
+                break;
+            case SCENE_MODE_TEST_PLAYER:
+                test_player_loop();
+                break;
+        }
         EndDrawing();
     }
 }
