68ec297f05b048ac1432300271e9239cecd94e95
Luis Dietl <interruptedThread@gmail.com>
2025-01-24 23:18:47 +0100

    things

--- code/game.cpp
+++ code/game.cpp
@@ -7,6 +7,10 @@ typedef uint8_t u8;
 typedef uint32_t u32;
 typedef int32_t i32;
 
+#define ROOM_WIDTH 20
+#define ROOM_HEIGHT 15
+#define TILE_SIZE 100
+
 enum Tile
 {
     Tile_Empty,
@@ -15,7 +19,7 @@ enum Tile
 
 struct Room
 {
-    Tile tiles[20 * 15];
+    Tile tiles[ROOM_WIDTH * ROOM_HEIGHT];
 };
 
 i32 main()
@@ -24,11 +28,39 @@ i32 main()
     InitWindow(1600, 900, "Divegame");
     SetTargetFPS(60);
 
+    Room room = {};
+    for (u32 x = 0; x < 20; ++x)
+    {
+        for (u32 y = 0; y < 15; ++y)
+        {
+            Tile tile = Tile_Empty;
+
+            if (x >= 3 && x <= 5 && y >= 7 && y <= 8)
+            {
+                tile = Tile_Wall;
+            }
+
+            room.tiles[x + y * ROOM_WIDTH] = tile;
+
+        }
+    }
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawRectangle(100, 100, 100, 100, RED);  
+
+        for (u32 x = 0; x < ROOM_WIDTH; ++x)
+        {
+            for (u32 y = 0; y < ROOM_HEIGHT; ++y)
+            {
+                if (room.tiles[x + y * ROOM_WIDTH])
+                {
+                    DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, RED);  
+                }
+            }
+        }
+
         EndDrawing();
     }
 }
